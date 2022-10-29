1c18363e3107ec303ced60dd0ec7d4f144202163
Kevin Wallen <46876927+daisukedd@users.noreply.github.com>
2022-10-29 17:58:21 -0300

    build: setup react-native

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Kevin "Daisuke" Wallen</Text>
+      <Text>Design | Broadcast Developer | Spec</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 25,
+  }
+});
