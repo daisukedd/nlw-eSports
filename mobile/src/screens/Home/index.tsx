8bdbc3260d0d9bfc3437cfe135a209a439f53380
Kevin Wallen <46876927+daisukedd@users.noreply.github.com>
2022-10-30 16:23:06 -0300

    style: home page, logo and heading
    wallpaper e logo no inicio da aplicação e heading

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,21 @@
+import { View, Image } from 'react-native';
+
+import { styles } from './styles';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image 
+        source={logoImg}
+        style={styles.logo} 
+      />
+      <Heading
+        title={'Encontre seu duo!'}
+        subtitle={'Selecione o game \u{1F3AE} que deseja jogar...'}
+      />
+    </View>
+  );
+}
